368368021dea11faef1ccebfe09a90109e0e5af0
ndula Perry <perryndula872@gmail.com>
2025-08-14 09:29:01 +0100

    first commit

---
+++ day1.py
@@ -0,0 +1,100 @@
+# print("helloo world")
+
+# num = 10000030033333
+# num1 = 20
+# num2= 8
+# num4 = "me"
+# print(f"{num1}, {num2}, {num4}")
+
+
+
+# print(num1)
+
+# x = 1
+# X = 3
+# v_4 = 6
+
+# age = 19
+# Age = 20
+# print(Age)
+
+# num_students = 20
+# num_pens = 49
+
+#this is a comment
+# print("perry") 
+# """ hsjsjhdjdhdjjshjdhdh
+# hdjdhdgdjdgjgfjh
+# djdgfjfgfj
+
+# def greet():
+#     """this function is use when greeting a user."""
+#     print("hello how are you doing")
+# greet()
+# print(greet.__doc__) 
+# def add(a,b):
+#     """"it takes twp parameters
+#     return the their sum"""
+#     return a +b 
+# print(add(3,5))
+# print(add.__doc__)
+
+# prompt = "Hey, what's up? "
+# user_input = input(prompt)
+# print("You said:", user_input)
+
+# name = 7
+# score = 99.847484
+# print("student: %i scored %.2f%% in his final exam" %(name, score))
+# print("it is %s that %s is in Africa" %(True, "cameroon"))
+
+# print("we have {n} student and each will score atleast {s:.3f}".format(n=20,s=70.7378))
+
+# name = "john"
+# score = 59.9373
+# print(f"student: {name} score: {score:.3f}")
+# print(f"{name} and {score**4}")
+
+# name = "perry"
+# score = 99.847484
+# print("student: %s scored %.2f%% in his final exam" % (name, score))
+# print("it is %s that %s is in Africa" % (True, "Cameroon"))
+
+# x = 10
+# print(x)
+# x += 2 # add 2 to the value of x and save the answer as the new value of x
+# # print(x)
+# # x -= 2 #substract 2 and update the value of x
+# # print(x)
+# x /= 4 # divide x by 4 and update the value of x
+# print(x)
+
+# # #equality (==)
+# # print(89==67)
+# # # # not equal to (!=)
+# # print(89 !=67)
+# # # #greater than (>)
+# # print (89 > 67)
+# # # #less than (<)
+# # print(89<67)
+# # # #greater than or equal to (>=)
+# print(89>=67)
+# # #less than or equal to (<=)
+# print(89<=67)
+
+# a = tuple([1,2,3])
+# b = ['a','b','c']
+# c = tuple([1,2,3])
+# print(type(a), type(c))
+# print(id(a), id(c))
+# # print(a is b)
+# # print(a is c)
+# # print(a is not c)
+
+# a = 2
+# b = 4 
+# c = 2
+# print(a is b)
+# print(a is c)
+# print(a is not c)
+#
---
+++ day2.py
@@ -0,0 +1,111 @@
+# print("hello world")
+# print(30)
+# print("how are you doing ")
+# perry = 78
+# print(perry)
+# a = 10 
+# print(a)
+# name = "alice"
+# print(name)
+# Perry = 7
+# perry = 10
+# print(perry)
+
+# name = "Nelson"
+# print(name)
+# User_name = "kien"
+# _name = 8
+# # %name = 50
+# num_students = 70
+# blessing_9 = 60
+# name#me = 
+# name9me = 
+# x = 60
+# num_eggs = 60
+
+# age = 30.76348
+# name = "perry"
+# print(f"my name is {name} and i am {age} years old  ")
+# print(f"my name is {name} and i am {age:.2f} years old  ")
+
+# age = 10.7387368
+# approx_age = round(age)
+# print(approx_age)
+# scrore = 70
+# name = "Edmund"
+# print(f"my name is {name} i am {age:.3f} i scored {scrore:.2f}")
+# print(f"my name is {name} i am {approx_age} i scored {scrore:.2f}")
+
+# x = 5
+# y = 9
+# c = (x + y) - x * (x%y)/2
+# print(c)
+# x = 6 
+# y = 9
+# print(x>y)
+# print(y>x)
+# while y > x:
+#     print(y)
+#     y -= 1
+
+# # print(True or True)
+
+# name = "perry"
+# # print('e'in name)
+# print("E" in name)
+
+
+#data type 
+#numeric data types 
+#integers, floats and complex numbers
+# x = 4 
+# y = -7
+# print(type(x))
+# print(type(y))
+# x = 4.3
+# print(type(x))
+# y = 0.7
+# print(type(y))
+# x = "20"
+# print(type(x))
+# x = 4 
+
+# x = 6 
+# y = 2.5
+# print(x+y)#8.5
+# print(x-y)#4.5, 3.5
+# print(x*y)#15
+# print(x%y)
+# print(x/y)#
+# print(x//y)#
+# x = 7.9
+# y = int(x) int()
+# print(y)
+# x = "20"
+# y = int(x)
+# print(type(y))
+# print(type(x))
+
+#complex data type
+# a = 3 +2j
+# print(type(a))
+# a = 3 + 0j
+# print(type(a))
+# a = 2 +3j 
+# # print(type(a))
+# # print(a.real)
+# # print(a.imag)
+# print(f"the real part of the complex number is {a.real}")
+# print(f"the imaginary part of the complex number is {a.imag}")
+
+# x = 1 + 4j
+# t = 2 + 5j
+# c = x + t
+# #print(c) 1*2 +1*5j +4j*2 + 4j*5j
+# d = x * t
+# print(d)
+# c = x / t
+# print(t)
+ 
+# c = x // t 
+# print(c)
