fb2838c4a78dafcea26f9b646bfd07f00f4d79c9
ndula Perry <perryndula872@gmail.com>
2025-08-16 01:56:25 +0100

    second

---
+++ day3.py
@@ -0,0 +1,63 @@
+#sequence data type 
+
+#string
+# city = "Buea"
+# print(type(city))
+# name = "gjdsksjhfsjkldagjsfjldjhsfjldjhsfjkdjh"
+# print(len(name))
+
+# print(city[0])
+# print(city[-1])
+# print(city[1:3])
+# print(city[1:])
+# print(city[:2])
+
+# first = "perry"
+# second = "ndula"
+# # print(first, second)
+# full_name = first +" "+ second
+# print(full_name)
+# def loop():
+#     for i in full_name: 
+#         print(i)
+# loop()
+
+#LIST
+# list1 = ["one", 10, 15.5, "10.1.1.0/28"]
+# print(list1)
+# print(type(list1))
+
+# print(list1[1:])
+# nasted_list = [1,3,4,["one","two", "three"], 4]
+# print(nasted_list)
+# print(nasted_list[4])
+# print(nasted_list[0][1])
+# print(nasted_list[3])
+# print(nasted_list[3][1])
+
+list2 = ["edmund", "kien", "nelson"]
+# list2.append("perry")
+# print(list2)
+# list2.insert(1, "perry")
+# print(list2)
+# list2.remove("edmund")
+# print(list2)
+
+# pop_remove = list2.pop(0)
+# print(pop_remove)
+# print(list2)
+# del list2[1]
+# print(list2)
+# list2.clear()
+# print(list2)
+
+# list1 = [1,3,4,6, 9]
+# list2 = [10, 20, 30, 40]
+
+# # list1.extend(list2)
+# # print(list1)
+
+# list3 = list1 + list2
+# print(list3)
+# list4 = list1.copy()
+# print(list4)
