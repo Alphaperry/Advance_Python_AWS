bb17760c4f44130526b082d42ada4bddc3adb642
ndula Perry <perryndula872@gmail.com>
2025-09-03 10:16:18 +0100

    day7

---
+++ day7.py
@@ -0,0 +1,70 @@
+#control flow 
+
+# conditional statements
+
+# if  condition:
+#     block1
+#     block2
+
+# num = 4 
+# if num > 5:
+#      print(num)
+
+# num = 3
+# if num > 5:
+#     print(num)
+# else:
+#     num +=1
+#     print(num)
+
+# grade = 50
+
+# if grade > 80:
+#     print("A")
+# elif grade > 70:
+#     print("B")
+# elif grade > 60:
+#     print("C")
+# else:
+#     print("D")
+
+
+
+# grade = 40
+
+# # if grade >20 and grade<30:
+# #     print("D")
+# # elif grade > 30 and grade <40:
+# #     print("C")
+# # elif grade > 40 and grade < 50:
+# #     print("B")
+# # else:
+# #     print("A")
+
+
+# if grade >20 and grade<30:
+#     print("D")
+# if grade > 30 and grade <40:
+#     print("C")
+# if grade >= 40 and grade < 50:
+#     print("B")
+# else:
+#     print("A")
+
+# fruits = ["mango", "apple", "banana"]
+
+# # if fruits == "apple":
+# #     print(fruits)
+
+# # if "apple" in fruits:
+# #     print("apple")
+
+# num = -9
+# if num >0:
+#     print(f"{num} is positive")
+#     if num %2 == 0:
+#         print(f"{num} is even")
+#     else:
+#         print(f"{num} is negative")
+# # else:
+# #     print(f"{num} is negative")
